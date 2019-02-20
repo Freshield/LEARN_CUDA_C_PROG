283cdaecd3ee57c375c7b9ba257a37fb1e283ca1
freshield <zxdsw199182@gmail.com>
2019-02-20 16:36:20 +0800

    done test using clion

---
+++ chp1/chp1_test/cmake-build-debug/CMakeFiles/chp1_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chp1_test.dir/chp1_test_generated_main.cu.o"
+  "chp1_test.pdb"
+  "chp1_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/chp1_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
